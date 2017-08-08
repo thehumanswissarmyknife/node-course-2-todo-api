eede3af3f7c74b66493f9c0572595b3ec0f14d1e
Dennis Vocke <dennisvocke@gmail.com>
2017-08-08 14:18:48 +0000

    tests for /todo:id added

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,14 +1,18 @@
 const expect = require('expect');
 const request = require('supertest');
+var {ObjectID} = require('mongodb');
 
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
 const todos = [{
+	_id: new ObjectID(), 
 	text: 'First todo'
 }, {
+	_id: new ObjectID(),
 	text: 'second todo'
 }, {
+	_id: new ObjectID(),
 	text: 'Third todo'
 }];
 
@@ -74,3 +78,29 @@ describe('GET /todos', () => {
 		.end(done);
 	});
 }); 
+
+describe('GET /todo/:id', () => {
+	it('should catch badly formed ids with a 404', (done) => {
+		request(app)
+		.get(`/todo/123`)
+		.expect(404)
+		.end(done);
+	});
+
+	it('should throw 404 when no todo is found', (done) => {
+		request(app)
+		.get(`/todos/${new ObjectID().toHexString}/123`)
+		.expect(404)
+		.end(done);
+	});
+
+	it('should return todo, if it\'s found', (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text)
+			})
+			.end(done);
+	});
+});
